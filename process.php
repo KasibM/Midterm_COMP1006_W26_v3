36f8c1da3655435488b78bb644da7b361bf0ee79
KasibM <129322185+KasibM@users.noreply.github.com>
2026-03-06 13:00:20 -0500

    added sanitisation

--- process.php
+++ process.php
@@ -9,6 +9,13 @@
 
     }
 
+    //sanitise input
+    $title = trim(filter_input(INPUT_POST, '',FILTER_SANITIZE_SPECIAL_CHARS));
+    $author = trim(filter_input(INPUT_POST, '',FILTER_SANITIZE_SPECIAL_CHARS));
+    $rating = trim(filter_input(INPUT_POST, '',FILTER_SANITIZE_SPECIAL_CHARS));
+    $review_text = trim(filter_input(INPUT_POST, '',FILTER_SANITIZE_SPECIAL_CHARS));
+
+    //
 
 
 
