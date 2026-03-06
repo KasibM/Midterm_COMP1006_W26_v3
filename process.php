0dfa127ce3507ac442be140da4354ddfc9a206cd
KasibM <129322185+KasibM@users.noreply.github.com>
2026-03-06 13:14:23 -0500

    validation and error display added

--- process.php
+++ process.php
@@ -15,9 +15,39 @@
     $rating = trim(filter_input(INPUT_POST, '',FILTER_SANITIZE_SPECIAL_CHARS));
     $review_text = trim(filter_input(INPUT_POST, '',FILTER_SANITIZE_SPECIAL_CHARS));
 
-    //
+    //vallidation
 
+    $errors = [];
 
+    if($title === null || $title === ''){
+        $errors[] = "title is required.";
+    }
+
+    if($author === null || $author === ''){
+        $errors[] = "author is required.";
+    }
+
+    if($rating === null || $rating === ''){
+        $errors[] = "rating is required.";
+    } else if (!filter_var($rating, FILTER_VALIDATE_INT)){
+        $errors[] = "rating must be an integer."
+    } else if (!($rating > 0 && $rating < 6)){
+        $errors[] = "rating must be a number from 1 to 6.";
+    }
+
+    if($review_text === null || $review_text === ''){
+        $errors[] = "review_text is required.";
+    }
+    
+    //if invalid list errors and exit code
+    if (!empty($errors)) { ?>
+    <p>Failed to insert data due to the following errors:</p>
+    <?php foreach ($errors as $error) : ?>
+        <li><?= $error ?> </li>
+    <?php endforeach;
+    //stop the script from executing  
+    exit;
+}
 
 
 
