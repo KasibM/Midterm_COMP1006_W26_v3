05c51d54047c3b54764bd705f47ec4c9916f84ce
KasibM <129322185+KasibM@users.noreply.github.com>
2026-03-06 12:56:05 -0500

    moved require connect.php from index to process

--- process.php
+++ process.php
@@ -1,3 +1,18 @@
 <?php 
+    //connect to database
+    require "connect.php";
+
+
+    //check if method is post
+    if($_SERVER['REQUEST_METHOD']!=='POST'){
+        die('Invalid request!');
+
+    }
+
+
+
+
+
+
 
 ?>
